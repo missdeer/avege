40b5ce3e5bdd89590565bf70e75fdc3a985af304
Fan Yang <missdeer@dfordsoft.com>
2017-01-17 15:00:15 +0800

    (*)use standard package to parse CIDR

--- src/outbound/ss/dial.go
+++ src/outbound/ss/dial.go
@@ -1,10 +1,7 @@
 package ss
 
 import (
-	"encoding/binary"
 	"net"
-	"strconv"
-	"strings"
 	"time"
 
 	"common"
@@ -31,53 +28,24 @@ func setLocalAddress(priorityInterfaceAddress string) {
 		return
 	}
 
-	pia := strings.Split(priorityInterfaceAddress, "/")
-	if len(pia) != 2 {
-		common.Warning("unexpected priority interface address format, something like 192.168.1.0/24 is expected.")
+	_, priorityInterfaceAddressIPNet, e := net.ParseCIDR(priorityInterfaceAddress)
+	if e != nil {
+		common.Error("parsing priority interface address failed", priorityInterfaceAddress, e)
 		return
 	}
-
-	// ipv4 only
-	piaddr := net.ParseIP(pia[0])
-	if piaddr == nil || piaddr.To4() == nil {
-		common.Warning("incorrect IP for priority interface address, only IPv4 is supported currently.", pia[0])
-		return
-	}
-
-	mask, err := strconv.Atoi(pia[1])
-	if err != nil {
-		common.Warning("incorrect network address mask, 1-32 is required", pia[1])
-		return
-	}
-
-	piaddrInt32 := binary.BigEndian.Uint32(piaddr.To4())
-	var finalMask uint32
-	for i := 0; i < mask; i++ {
-		var bit uint32 = 0x80000000
-		bit >>= uint(i)
-		finalMask |= bit
-	}
-	piaddrInt32 &= finalMask
 
 	addrs, err := net.InterfaceAddrs();
 	if err != nil {
 		common.Warning("can't get interface addresses", err)
 		return
 	}
-	for _, addr := range addrs {
-		if a := strings.Split(addr.String(), "/"); len(a) == 2 {
-			ip := net.ParseIP(a[0])
-			if ip == nil || ip.To4() == nil {
-				continue
-			}
 
-			ipInt32 := binary.BigEndian.Uint32(ip.To4()) & finalMask
-			if ipInt32 == piaddrInt32 {
-				tcpAddr = &net.TCPAddr{
-					IP: ip,
-				}
-				break
+	for _, addr := range addrs {
+		if ip, _, e := net.ParseCIDR(addr.String()); e == nil && priorityInterfaceAddressIPNet.Contains(ip) {
+			tcpAddr = &net.TCPAddr{
+				IP: ip,
 			}
+			break
 		}
 	}
 }
