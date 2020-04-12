4c550d0b4f037dbbdff4f3a89823c32008e13928
Fan Yang <missdeer@dfordsoft.com>
2020-04-12 11:01:40 +0800

    (*)fix chacha20 import path

--- outbound/ss/encrypt.go
+++ outbound/ss/encrypt.go
@@ -13,7 +13,7 @@ import (
 	"github.com/missdeer/avege/common"
 	"github.com/missdeer/avege/common/ds"
 
-	"github.com/Yawning/chacha20"
+	"gitlab.com/yawning/chacha20.git"
 	"github.com/dgryski/go-camellia"
 	"github.com/dgryski/go-idea"
 	"github.com/dgryski/go-rc2"
@@ -96,11 +96,11 @@ func newRC4MD5Stream(key, iv []byte, _ DecOrEnc) (cipher.Stream, error) {
 }
 
 func newChaCha20Stream(key, iv []byte, _ DecOrEnc) (cipher.Stream, error) {
-	return chacha20.NewCipher(key, iv)
+	return chacha20.New(key, iv)
 }
 
 func newChacha20IETFStream(key, iv []byte, _ DecOrEnc) (cipher.Stream, error) {
-	return chacha20.NewCipher(key, iv)
+	return chacha20.New(key, iv)
 }
 
 type salsaStreamCipher struct {
